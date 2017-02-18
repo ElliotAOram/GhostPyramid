86b814be432b55bd1a2aa45531ccb491a53248ed
ElliotAOram <elo9@aber.ac.uk>
2017-02-18 16:45:18 +0000

    Create tests for VideoProcessor class
    Refs #25

---
+++ VideoProcessingApplication/VideoProcessorTest.py
@@ -0,0 +1,31 @@
+import unittest
+from VideoProcessor import VideoProcessor
+
+class testCameraCapture(unittest.TestCase):
+
+	###------------------------------setUp and tearDown--------------------------------###
+	def setUp(self):
+		self.vp = VideoProcessor()
+
+	def tearDown(self):
+		self.widget = None
+
+	###---------------------------------Success cases----------------------------------###
+	def test_video_feed_is_initially_null(self):
+		self.assertIsNone(self.vp.get_video_feed())
+
+	def test_begin_capture_from_webcam(self):
+		self.vp.begin_capture(0) # Starts capture with zeroth device (webcam)
+		self.assertIsNotNone(self.vp.get_video_feed())
+
+	def test_end_capture_from_webcam(self):
+		self.vp.begin_capture(0)
+		self.vp.end_capture()
+		self.assertIsNone(self.vp.get_video_feed())
+
+	###---------------------------------Failure cases---------------------------------###
+	def test_non_int_value_to_begin_capture_function(self):
+		self.vp.begin_capture('Not an int')
+
+if __name__ == '__main__':
+    unittest.main()

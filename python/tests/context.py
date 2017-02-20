c1f59287c35e41bfbbd8a6f2b69421927e8c5c81
ElliotAOram <elo9@aber.ac.uk>
2017-02-20 16:50:29 +0000

    Pylint: Add docstring, pylint disables and justifaction to .context file
    Refs #25

--- python/tests/context.py
+++ python/tests/context.py
@@ -1,3 +1,9 @@
+"""Allows tests to find the VideoProcessor module for testing
+   This is required as the modules are in different directories"""
+#pylint: disable=wrong-import-position,unused-import
+#   Above pylint disables are required as this is a context file and
+#   is only used by other files to set namespace and resovle modules.
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))

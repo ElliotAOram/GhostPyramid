57647590e39d244a2112a838141b7921e238523d
ElliotAOram <elo9@aber.ac.uk>
2017-02-20 16:02:51 +0000

    Pylint: Fix pylint issues in video_processor file
    Refs #25

--- python/vpa/video_processor.py
+++ python/vpa/video_processor.py
@@ -1,37 +1,45 @@
-import numpy as np
-import cv2
+"""Contains the VideoProcessor class (core application)"""
+from cv2 import VideoCapture
 
-class VideoProcessor:
 
-	# Global variable for video feed
-	video_feed = None
+class VideoProcessor(object):
+    """
+    @class VideoProcessor   :: Handles the flow of the project from video
+                               input capture to video output.
+    """
 
-	'''
-	Begins capturing video from an external device and updates
-	the video_feed object with the camera output.
+    def __init__(self):
+        self.video_feed = None
 
-	@param device		:: The integer that corresponds to the device
-	@Raises ValueError 	:: Raises ValueError on non integer input
-	'''
-	def begin_capture(self, device):
-		try:
-			integer = int(device)
-		except ValueError:
-			raise ValueError('Value provided is not an integer')
 
-		self.video_feed = cv2.VideoCapture(device)
+    def begin_capture(self, device):
+        """
+        Begins capturing video from an external device and updates
+        the video_feed object with the camera output.
 
-	'''
-	Ends capturing video from the current video_feed. Sets the video_feed
-	object make to None to avoid misuse.
-	'''
-	def end_capture(self):
-		self.video_feed.release()
-		self.video_feed = None
+        @param device		:: The integer that corresponds to the device.
+        @Raises ValueError 	:: Raises ValueError on non integer input.
+        """
+        try:
+            int(device)
+        except ValueError:
+            raise ValueError('Value provided is not an integer')
 
-	'''
-	Returns the video_feed currently in use.
-	@return video_feed		:: The current video feed from a camera
-	'''
-	def get_video_feed(self):
-		return self.video_feed
+        self.video_feed = VideoCapture(device)
+
+
+    def end_capture(self):
+        """
+        Ends capturing video from the current video_feed.
+        Sets the video_feed object make to None to avoid misuse.
+        """
+        self.video_feed.release()
+        self.video_feed = None
+
+
+    def get_video_feed(self):
+        """
+        Returns the video_feed currently in use.
+        @return video_feed      :: The current video feed from a camera.
+        """
+        return self.video_feed

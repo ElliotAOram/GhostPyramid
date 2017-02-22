78207e887494747bd136b4ecd4ac700192e3e3d4
ElliotAOram <elo9@aber.ac.uk>
2017-02-22 19:05:04 +0000

    Add test stubs for calculate_image_positions
    Refs #31

--- python/tests/test_helper_functions.py
+++ python/tests/test_helper_functions.py
@@ -5,7 +5,7 @@ from .context import helperfunctions
 
 class TestScreenBoundaries(unittest.TestCase):
     """
-    @class TestScreenBoundaries     :: Test the function that provides the maximum 
+    @class TestScreenBoundaries     :: Test the function that provides the maximum
                                        screen boundaries for the display
     """
     ###------------------------------setUp and tearDown--------------------------------###
@@ -42,3 +42,43 @@ class TestScreenBoundaries(unittest.TestCase):
         """
         Ensures that the function produces a value error if integers are not supplied
         """
+
+
+class TestCalculateImagePositions(unittest.TestCase):
+    """
+    @class TestCalculateImagePositions      :: Test the function that positions the image
+                                               in the correct area of the frame
+    """
+    ###------------------------------setUp and tearDown--------------------------------###
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'teardown'
+
+    ###---------------------------------Success cases----------------------------------###
+    def test_normal_image_size(self):
+        """
+        Test that a normal size image is placed correctly.
+        This test will check the boundaries of the image.
+        """
+        
+
+    ###---------------------------------Failure cases----------------------------------###
+    def test_non_int_screen_length(self):
+        """
+        Ensure function raises a ValueError if screenlength is non int
+        """
+        
+
+    def test_non_int_width(self):
+        """
+        Ensure function raises a ValeError is width is non int
+        """
+    
+
+    def test_non_int_height(self):
+        """
+        Ensure function raises a ValueError is height is non int
+        """
+    

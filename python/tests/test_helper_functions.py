7b452701e6fbc9bacb743cbb410fd290a5ac728a
ElliotAOram <elo9@aber.ac.uk>
2017-02-22 20:17:33 +0000

    Implement tests calculate_screen_boundaries
    Refs #31

--- python/tests/test_helper_functions.py
+++ python/tests/test_helper_functions.py
@@ -21,12 +21,20 @@ class TestScreenBoundaries(unittest.TestCase):
         Tests that the correct square is calculate for supplied values were width
         is more than height.
         """
-
+        coordinates = calculate_screen_boundaries(1000, 500)
+        self.assertEqual((250,0), coordinates[0])
+        self.assertEqual((750,500), coordinates[1])
+        
+        
     def test_height_mt_width(self):
         """
         Tests that the correct square is calculate for supplied values were height
         is more than width.
         """
+        coordinates = calculate_screen_boundaries(500,1000)
+        self.assertEqual((0,250), coordinates[0])
+        self.assertEqual((500,750), coordinates[1])
+        
 
     def test_output_with_no_params(self):
         """
@@ -34,7 +42,11 @@ class TestScreenBoundaries(unittest.TestCase):
         can't be tested here as it will dependant on the machnine that it is run on, but
         type and format can be checked.
         """
-
+        coordinates = calculate_screen_boundaries()
+        self.assertIsInstance(coordinates[0][0], int)
+        self.assertIsInstance(coordinates[0][1], int)
+        self.assertIsInstance(coordinates[1][0], int)
+        self.assertIsInstance(coordinates[1][1], int)
 
     ###---------------------------------Failure cases----------------------------------###
 
@@ -42,6 +54,28 @@ class TestScreenBoundaries(unittest.TestCase):
         """
         Ensures that the function produces a value error if integers are not supplied
         """
+        self.assertRaises(ValueError,
+                          calculate_screen_boundaries,
+                          "not int",
+                          "not int")
+    
+    def test_raise_ValueError_with_0_width(self):
+        """
+        Ensures that the function produces a ValueError if a zero is provided
+        """
+        self.assertRaises(ValueError,
+                          calculate_screen_boundaries,
+                          0,
+                          100)
+
+    def test_raise_ValueError_with_0_height(self):
+        """
+        Ensures that the function produces a ValueError if a zero is provided
+        """
+        self.assertRaises(ValueError,
+                          calculate_screen_boundaries,
+                          100,
+                          0)
 
 
 class TestCalculateImagePositions(unittest.TestCase):

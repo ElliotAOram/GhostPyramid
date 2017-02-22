4ba41c33ce6e7b364a6b7eab14db44aeba7ae4c9
ElliotAOram <elo9@aber.ac.uk>
2017-02-22 21:35:43 +0000

    Add test cases for calc_image_positions and parsers
    Refs #31

--- python/tests/test_helper_functions.py
+++ python/tests/test_helper_functions.py
@@ -22,19 +22,17 @@ class TestScreenBoundaries(unittest.TestCase):
         is more than height.
         """
         coordinates = calculate_screen_boundaries(1000, 500)
-        self.assertEqual((250,0), coordinates[0])
-        self.assertEqual((750,500), coordinates[1])
-        
-        
+        self.assertEqual((250,0)    , coordinates[0])
+        self.assertEqual((750,500)  , coordinates[1])
+
     def test_height_mt_width(self):
         """
         Tests that the correct square is calculate for supplied values were height
         is more than width.
         """
         coordinates = calculate_screen_boundaries(500,1000)
-        self.assertEqual((0,250), coordinates[0])
-        self.assertEqual((500,750), coordinates[1])
-        
+        self.assertEqual((0,250)    , coordinates[0])
+        self.assertEqual((500,750)  , coordinates[1])
 
     def test_output_with_no_params(self):
         """
@@ -50,7 +48,7 @@ class TestScreenBoundaries(unittest.TestCase):
 
     ###---------------------------------Failure cases----------------------------------###
 
-    def test_raise_ValueError_with_non_int(self):
+    def test_non_int_zero_negative_input(self):
         """
         Ensures that the function produces a value error if integers are not supplied
         """
@@ -58,24 +56,16 @@ class TestScreenBoundaries(unittest.TestCase):
                           calculate_screen_boundaries,
                           "not int",
                           "not int")
-    
-    def test_raise_ValueError_with_0_width(self):
-        """
-        Ensures that the function produces a ValueError if a zero is provided
-        """
+
         self.assertRaises(ValueError,
                           calculate_screen_boundaries,
                           0,
                           100)
 
-    def test_raise_ValueError_with_0_height(self):
-        """
-        Ensures that the function produces a ValueError if a zero is provided
-        """
         self.assertRaises(ValueError,
                           calculate_screen_boundaries,
                           100,
-                          0)
+                          -1)
 
 
 class TestCalculateImagePositions(unittest.TestCase):
@@ -96,23 +86,113 @@ class TestCalculateImagePositions(unittest.TestCase):
         Test that a normal size image is placed correctly.
         This test will check the boundaries of the image.
         """
-        
+        coord_map = calculate_image_positions(500, 100, 100)
+        self.assertEqual((200,0)    , coord_map["top"][0])
+        self.assertEqual((300,100)  , coord_map["top"][1])
+        self.assertEqual((0,200)    , coord_map["left"][0])
+        self.assertEqual((100,300)  , coord_map["left"][1])
+        self.assertEqual((200,400)  , coord_map["bottom"][0])
+        self.assertEqual((300,500)  , coord_map["bottom"][1])
+        self.assertEqual((400,200)  , coord_map["right"][0])
+        self.assertEqual((500,300)  , coord_map["right"][1])
+
 
     ###---------------------------------Failure cases----------------------------------###
-    def test_non_int_screen_length(self):
+    def test_non_int_zero_negative_input(self):
         """
         Ensure function raises a ValueError if screenlength is non int
         """
-        
+        self.assertRaises(ValueError,
+                          calculate_image_positions,
+                          "not int", 1, 1)
 
-    def test_non_int_width(self):
-        """
-        Ensure function raises a ValeError is width is non int
-        """
-    
+        self.assertRaises(ValueError,
+                          calculate_image_positions,
+                          1, 0, 1)
 
-    def test_non_int_height(self):
-        """
-        Ensure function raises a ValueError is height is non int
-        """
-    
+        self.assertRaises(ValueError,
+                          calculate_image_positions,
+                          1, 1, -1)
+
+
+
+
+##Input parser function tests
+
+
+
+class TestPositiveParser(unittest.TestCase):
+    """
+    @class TestPositiveParser      :: Tests that ensure the input parser only accepts
+                                      postive integers
+    """
+    ###------------------------------setUp and tearDown--------------------------------###
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'teardown'
+
+    ###---------------------------------Success cases----------------------------------###
+    def test_integer_zero(self):
+        self.assertTrue(parse_positive_int(0))
+
+    def test_positive(self):
+        self.assertTrue(parse_positive_int(10))
+
+    ###---------------------------------Failure cases-----------------------------------###
+
+    def test_raise_ValueError_on_negative(self):
+        self.assertRaises(ValueError,
+                          parse_positive_int,
+                          -1)
+
+class TestNonZeroParser(unittest.TestCase):
+    """
+    @class TestPositiveParser      :: Tests that ensure the input parser only accepts
+                                      non zero integers
+    """
+    ###------------------------------setUp and tearDown--------------------------------###
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'teardown'
+
+    ###---------------------------------Success cases----------------------------------###
+    def test_integer_zero(self):
+        self.assertTrue(parse_non_zero_int(-10))
+
+    def test_positive(self):
+        self.assertTrue(parse_non_zero_int(10))
+
+    ###---------------------------------Failure cases-----------------------------------###
+
+    def test_raise_ValueError_on_zero(self):
+        self.assertRaises(ValueError,
+                          parse_non_zero_int,
+                          0)
+
+class TestIntParser(unittest.TestCase):
+    """
+    @class TestPositiveParser      :: Tests that ensure the input parser only accepts
+                                      postive integers
+    """
+    ###------------------------------setUp and tearDown--------------------------------###
+    def setUp(self):
+        print 'setup'
+
+    def tearDown(self):
+        print 'teardown'
+
+    ###---------------------------------Success cases----------------------------------###
+    def test_integer_zero(self):
+        self.assertTrue(parse_int(0))
+
+
+    ###---------------------------------Failure cases-----------------------------------###
+
+    def test_raise_ValueError_on_non_int(self):
+        self.assertRaises(ValueError,
+                          parse_int,
+                          "Non int")

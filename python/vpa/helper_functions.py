5c72bb690cedb3730e16e4903162056c02aee843
ElliotAOram <elo9@aber.ac.uk>
2017-02-23 14:17:04 +0000

    trim trailing whitespace
    Refs #31

--- python/vpa/helper_functions.py
+++ python/vpa/helper_functions.py
@@ -58,9 +58,9 @@ def calculate_screen_boundaries(width=None, height=None):
     Calculates the largest possible square and its co-ordinates in a
     provided rectangular space. This is used to establish the correct
     display area on a screen
-    @param width        :: default = None - Max width of the screen. 
+    @param width        :: default = None - Max width of the screen.
                            If not provided use screen resolution
-    @param height       :: default = None - Max height of the screen. 
+    @param height       :: default = None - Max height of the screen.
                            If not provided use screen resolution
     @return             :: Tuple representing the origin and bottom right
                            hand corner of the display area
@@ -94,13 +94,13 @@ def calculate_screen_boundaries(width=None, height=None):
 
 def calculate_image_positions(side_length, width, height):
     """
-    Calculates the positions (origin, bottom right) for each image in the 
+    Calculates the positions (origin, bottom right) for each image in the
     output window.
     @param side_length      :: Length of one side of the display area.
                                Area assumed to be square
     @param width            :: Width of the image
     @param height           :: Height of the image
-    @return                 :: A dictionary of image postions by origin 
+    @return                 :: A dictionary of image postions by origin
                                and bottom right tuples - for example:
                                {"top": [(25,0), (75,50)]
                                 "left ": [...]}
@@ -112,25 +112,25 @@ def calculate_image_positions(side_length, width, height):
     parse_positive_int(side_length)
     parse_positive_int(width)
     parse_positive_int(height)
-    
+
     # Find half values
     h_image_width = width / 2
     h_image_height = height / 2
     h_side_length = side_length / 2
-    
+
     # Define markers
     first_width = h_side_length-h_image_width
     second_width = h_side_length+h_image_width
     first_height = h_side_length-h_image_height
     second_height = h_side_length+h_image_height
-    
-    
+
+
     return {"top"   : [[first_width, 0] , [second_width, height]],
             "left"  : [[0, first_height], [width, second_height]],
             "bottom": [[first_width, side_length-height],[second_width, side_length]],
             "right" : [[side_length-width, first_height],[side_length, second_height]]
            }
-    
+
 def create_image_position_dictionary(screen_width, screen_height, frame_width, frame_height):
     """
     Calculates the position dictionary for images given a screen
@@ -155,5 +155,5 @@ def create_image_position_dictionary(screen_width, screen_height, frame_width, f
     for _, value in img_positions.iteritems():
         value[0][0] += displacement
         value[1][0] += displacement
-    
+
     return img_positions

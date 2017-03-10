7a366ccf85460170063dd1c96fa6ad42f22b9430
ElliotAOram <elo9@aber.ac.uk>
2017-03-10 19:38:13 +0000

    Add all activities (tests for landing pages to follow)
    Refs #42

--- Charades/app/src/main/java/dcs/aber/ac/uk/charades/MainActivity.java
+++ Charades/app/src/main/java/dcs/aber/ac/uk/charades/MainActivity.java
@@ -1,7 +1,9 @@
 package dcs.aber.ac.uk.charades;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends BlankActivity{
@@ -10,6 +12,28 @@ public class MainActivity extends BlankActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Starts the intent to go to the actor splash.
+     * Finish is called at the end of this page to ensure you can not return to this page.
+     * @param view :: The current activity view
+     */
+    public void toActorSplash(View view) {
+        Intent intent = new Intent(this, ActorLanding.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Starts the intent to go to the viewer splash.
+     * Finish is called at the end of this page to ensure you can not return to this page.
+     * @param view :: The current activity view
+     */
+    public void toViewerSplash(View view) {
+        Intent intent = new Intent(this, ViewerLanding.class);
+        startActivity(intent);
+        finish();
     }
 
 

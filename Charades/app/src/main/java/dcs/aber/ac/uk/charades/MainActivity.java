d30c8429259ee9728fcafe1d6b9e747b29c23079
ElliotAOram <elo9@aber.ac.uk>
2017-03-10 12:11:24 +0000

    Change main to import from BlankActivity
    Refs #42

--- Charades/app/src/main/java/dcs/aber/ac/uk/charades/MainActivity.java
+++ Charades/app/src/main/java/dcs/aber/ac/uk/charades/MainActivity.java
@@ -1,27 +1,15 @@
 package dcs.aber.ac.uk.charades;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
 
-    private View decorView;
+public class MainActivity extends BlankActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                      | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
     }
 
 

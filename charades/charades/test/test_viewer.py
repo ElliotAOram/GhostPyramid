f1b4a856f3ffab5409912ba5339c94081b90abb9
ElliotAOram <elo9@aber.ac.uk>
2017-03-24 22:40:33 +0000

    Remove trailing white space from test
    Refs #48

--- charades/charades/test/test_viewer.py
+++ charades/charades/test/test_viewer.py
@@ -7,17 +7,17 @@ class TestViewer(unittest.TestCase):
     def setUp(self):
         self.viewer_obj = Viewer()
 
-    def test_create_user(self):
+    def test_create_viewer(self):
         self.assertIsNotNone(self.viewer_obj)
         self.assertIsInstance(self.viewer_obj, type(Viewer))
 
     def test_get_points(self):
         self.assertEqual(self.viewer_obj.points, 0)
 
-    def test_increment_points(self):        
+    def test_increment_points(self):
         self.viewer_obj.increment_points(25)
         self.assertEqual(self.viewer_obj.points, 25)
-        
+
         self.viewer_obj.increment_points(10)
         self.assertEqual(self.viewer_obj.points, 35)
 
@@ -27,7 +27,6 @@ class TestViewer(unittest.TestCase):
     def test_set_name(self):
         viewer_obj.set_name('test-name')
         self.assertEqual(viewer_obj.name, 'test-name')
-        
+
     def tearDown(self):
         self.viewer_obj = None
-    

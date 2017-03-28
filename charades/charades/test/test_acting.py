fc3779bc84291c4c9d89409d7e6ddcc441e80d18
ElliotAOram <elo9@aber.ac.uk>
2017-03-28 18:10:55 +0100

    Add setUp to avoid redirects for test_acting
    Refs #50

--- charades/charades/test/test_acting.py
+++ charades/charades/test/test_acting.py
@@ -16,14 +16,19 @@ class ActingTests(StaticLiveServerTestCase):
         cls.browser = webdriver.Chrome()
         cls.browser.implicitly_wait(10)
 
+    def setUp(self):
+        self.browser.get('%s%s' % (self.live_server_url, '/instructions/?user_type=Actor'))
+        self.browser.refresh()
+
     def test_page_basic_page_elements(self):
         """
         Test the basic contents of the index
         """
-        self.browser.get('%s%s' % (self.live_server_url, '/acting/'))
+        self.browser.get('%s%s' % (self.live_server_url, '/acting/?phrase=Tennis'))
         self.assertTrue('Charades' in self.browser.title)
         page_title = self.browser.find_element_by_class_name('page_title')
         self.assertIsNotNone(page_title)
+        self.browser.refresh()
 
     def tearDown(self):
         self.browser.refresh()

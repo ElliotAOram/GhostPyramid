74feb5ad2c9ef839e071396961c901cb8739875d
ElliotAOram <elo9@aber.ac.uk>
2017-03-29 17:58:55 +0100

    Add warning to index and string
    Refs #50

--- charades/charades/strings.py
+++ charades/charades/strings.py
@@ -5,3 +5,7 @@ def actor_instructions():
 
 def viewer_instructions():
     return "This is the string for the viewer instructions."
+
+def actor_none():
+    return "Requested page could not be loaded as no actor is present." +\
+           "Please ensure an Actor has signed in using the correct session key."

bb4c5475c2ee460bd63984da7667a25cb1c6047b
ElliotAOram <elo9@aber.ac.uk>
2017-04-01 14:06:45 +0100

    Update instructions test + template to match design change
    This will require additional update for ensure the button leads to the correct loaction for the viewer when clicked
    
    Refs #54

--- charades/charades/test/test_instructions.py
+++ charades/charades/test/test_instructions.py
@@ -20,50 +20,46 @@ class InstructionsTests(StaticLiveServerTestCase):
 
     def test_generic_page_elements(self):
         """
-        test the non-actor and non-viewer spesific content
+        Test the non-actor and non-viewer spesific content
         """
         self.browser.get('%s%s' % (self.live_server_url,
                                    '/instructions/?session_id=BSW18'))
         self.assertTrue('Charades' in self.browser.title)
-        page_title = self.browser.find_element_by_class_name('page_title').text
-        self.assertEqual('Instructions', page_title)
+        self.assertEqual('Instructions',
+                         self.browser.find_element_by_class_name('page_title').text)
+        self.assertIsNotNone(self.browser.find_element_by_id('instructions_cont'))
+        self.assertEqual('Continue',
+                         self.browser.find_element_by_id('continue_button').get_attribute("value"))
 
     def test_page_elements_actor(self):
         """
-        test the elements of the actor page
+        Test the elements of the actor page
         """
         self.browser.get('%s%s' % (self.live_server_url,
                                    '/instructions/?session_id=BSW18&user_type=Actor'))
-        inst_para = self.browser.find_element_by_id('instruction_para').text
-        self.assertEqual(strings.actor_instructions(), inst_para)
-        actor_form = self.browser.find_element_by_id('phrase_selection')
-        self.assertIsNotNone(actor_form)
-        phrase_selection_button = self.browser.find_element_by_id('phrase_selection_button')
-        self.assertIsNotNone(phrase_selection_button)
+        self.assertEqual(strings.actor_instructions(),
+                         self.browser.find_element_by_id('instruction_para').text)
 
     def test_actor_button_click(self):
         """
-        test that the actor button for phrase selection
+        Test that the actor button for phrase selection
         moves the user to the phrase_selection.html page
         """
         self.browser.get('%s%s' % (self.live_server_url,
                                    '/instructions/?session_id=BSW18&user_type=Actor'))
-        phrase_sel_button = self.browser.find_element_by_id('phrase_selection_button')
+        phrase_sel_button = self.browser.find_element_by_id('continue_button')
         phrase_sel_button.click()
-        current_url = self.browser.current_url
-        self.assertTrue(self.live_server_url + '/select_phrase/' in current_url)
+        self.assertTrue(self.live_server_url + '/select_phrase/' in self.browser.current_url)
 
 
     def test_page_elements_viewer(self):
         """
-        test the elements of the viewer page
+        Test the elements of the viewer page
         """
         self.browser.get('%s%s' % (self.live_server_url,
                                    '/instructions/?session_id=BSW18&user_type=Viewer'))
-        inst_para = self.browser.find_element_by_id('instruction_para').text
-        self.assertEqual(strings.viewer_instructions(), inst_para)
-        please_wait = self.browser.find_element_by_id('please_wait_para').text
-        self.assertEqual(please_wait, "Please wait for the actor to select a phrase")
+        self.assertEqual(strings.viewer_instructions(),
+                         self.browser.find_element_by_id('instruction_para').text)
 
     def tearDown(self):
         self.browser.refresh()

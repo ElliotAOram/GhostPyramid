2de3a95ae797d2bc768bd95fda86d1545961e87b
ElliotAOram <elo9@aber.ac.uk>
2017-04-17 18:45:21 +0100

    Add string for new word
    Refs #65

--- charades/charades/strings.py
+++ charades/charades/strings.py
@@ -24,4 +24,7 @@ def invalid_session():
     return "Invalid session ID has been provided."
 
 def new_phrase():
-    return "The Viewers have guessed your phrase! Please select a new phrase from the list below"
+    return "The Viewers have guessed your phrase! Please select a new phrase from the list below."
+
+def new_word():
+    return "The Viewers have guessed your word! Please select a new word using the buttons below."
